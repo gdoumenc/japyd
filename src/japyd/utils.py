d977fb45115b535545587991217091c44fbcf947
gdoumenc <gdoumenc@fpr-coworks.com>
2026-02-23 15:53:07 +0100

    type added in flatten resource

--- src/japyd/utils.py
+++ src/japyd/utils.py
@@ -92,8 +92,8 @@ def flatten_resource(res: Resource | dict, *, more: dict | None = None) -> dict:
     """Returns the resource attributes with the 'id' added. Can add more data if needed."""
     more = more or {}
     if isinstance(res, Resource):
-        return {"id": res.id, **res.attributes, **more}
-    return {"id": res["id"], **res["attributes"], **more}
+        return {"type": res.type, "id": res.id, **res.attributes, **more}
+    return {"type": res["type"], "id": res["id"], **res["attributes"], **more}
 
 
 def to_bool(val: t.Any) -> bool:
